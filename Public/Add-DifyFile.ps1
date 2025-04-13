ec5e486c5d9e97a91e714b057ac228fbd1871233
kurokobo <2920259+kurokobo@users.noreply.github.com>
2025-04-13 10:29:50 +0900

    fix: split function to create temporaryfile for binary upload into independent function

--- Public/Add-DifyFile.ps1
+++ Public/Add-DifyFile.ps1
@@ -27,24 +27,11 @@ function Add-DifyFile {
 
         $UploadedFiles = @()
         foreach ($File in $Files) {
-
-            $Boundary = "----WebKitFormBoundary" + [System.Guid]::NewGuid().ToString("N")
-            $UTF8NoBOM = New-Object "System.Text.UTF8Encoding" -ArgumentList @($false)
-            $TemporaryFile = New-TemporaryFile
-            Write-Verbose "using $($TemporaryFile.FullName) as temporary file"
-
-            $FileStream = New-Object System.IO.FileStream($TemporaryFile, [System.IO.FileMode]::Append)
-            $BinaryWriter = New-Object System.IO.BinaryWriter($FileStream)
-
-            $BinaryWriter.Write($UTF8NoBOM.GetBytes("--$($Boundary)`r`n"))
-            $BinaryWriter.Write($UTF8NoBOM.GetBytes("Content-Disposition: form-data; name=`"file`"; filename=`"$($File.Name)`"`r`n"))
-            $BinaryWriter.Write($UTF8NoBOM.GetBytes("Content-Type: application/octet-stream`r`n`r`n"))
-            $BinaryWriter.Write([System.IO.File]::ReadAllBytes($File.FullName))
-            $BinaryWriter.Write($UTF8NoBOM.GetBytes("`r`n--$($Boundary)--`r`n"))
-            $BinaryWriter.Close()
+            $Result = New-TemporaryFileForBinaryUpload -File $File
+            $TemporaryFile = $Result.TemporaryFile
+            $ContentType = $Result.ContentType
 
             $Endpoint = Join-Url -Segments @($env:PSDIFY_URL, "/console/api/files/upload")
-            $ContentType = "multipart/form-data; boundary=$($Boundary)"
             $Method = "POST"
             if ($Source) {
                 $Query = @{
---
+++ Public/New-TemporaryFileForBinaryUpload.ps1
@@ -0,0 +1,28 @@
+function New-TemporaryFileForBinaryUpload {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [System.IO.FileInfo] $File
+    )
+
+    $Boundary = "----WebKitFormBoundary" + [System.Guid]::NewGuid().ToString("N")
+    $UTF8NoBOM = New-Object "System.Text.UTF8Encoding" -ArgumentList @($false)
+    $TemporaryFile = New-TemporaryFile
+
+    $FileStream = New-Object System.IO.FileStream($TemporaryFile, [System.IO.FileMode]::Append)
+    $BinaryWriter = New-Object System.IO.BinaryWriter($FileStream)
+
+    $BinaryWriter.Write($UTF8NoBOM.GetBytes("--$($Boundary)`r`n"))
+    $BinaryWriter.Write($UTF8NoBOM.GetBytes("Content-Disposition: form-data; name=`"file`"; filename=`"$($File.Name)`"`r`n"))
+    $BinaryWriter.Write($UTF8NoBOM.GetBytes("Content-Type: application/octet-stream`r`n`r`n"))
+    $BinaryWriter.Write([System.IO.File]::ReadAllBytes($File.FullName))
+    $BinaryWriter.Write($UTF8NoBOM.GetBytes("`r`n--$($Boundary)--`r`n"))
+    $BinaryWriter.Close()
+
+    $ContentType = "multipart/form-data; boundary=$($Boundary)"
+
+    return @{
+        TemporaryFile = $TemporaryFile
+        ContentType = $ContentType
+    }
+}
