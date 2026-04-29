b40c7afbf503333b71e6f6c80bfba8c555f3a162
kurokobo <2920259+kurokobo@users.noreply.github.com>
2026-04-29 14:10:25 +0900

    feat: bump tested dify version to 1.13.3

--- Tests/Invoke-PSDifyPester.ps1
+++ Tests/Invoke-PSDifyPester.ps1
@@ -24,7 +24,7 @@ $Environments = @(
     @{
         Id       = "community-release"
         Mode     = "community"
-        Version  = "1.13.2"
+        Version  = "1.13.3"
         Override = "compose_release.yaml"
         Env      = "env_release.env"
     },
