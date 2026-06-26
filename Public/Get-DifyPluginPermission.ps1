d4de2f5a8ea7de39a9aaf6cc9f34ec8f957b8ea6
kurokobo <kurokobo@protonmail.com>
2026-06-27 02:44:35 +0900

    feat: support 1.15.0 (#53)

--- Public/Get-DifyPluginPermission.ps1
+++ Public/Get-DifyPluginPermission.ps1
@@ -6,7 +6,12 @@ function Get-DifyPluginPermission {
         throw "You are not logged in to a Dify server yet, or the Dify server currently logged in does not support plugins."
     }
 
-    $Endpoint = Join-Url -Segments @($env:PSDIFY_URL, "/console/api/workspaces/current/plugin/preferences/fetch")
+    if (Compare-SimpleVersion -Version $env:PSDIFY_VERSION -Ge "1.15.0") {
+        $Endpoint = Join-Url -Segments @($env:PSDIFY_URL, "/console/api/workspaces/current/plugin/permission/fetch")
+    }
+    else {
+        $Endpoint = Join-Url -Segments @($env:PSDIFY_URL, "/console/api/workspaces/current/plugin/preferences/fetch")
+    }
     $Method = "GET"
     try {
         $Response = Invoke-DifyRestMethod -Uri $Endpoint -Method $Method -SessionOrToken $script:PSDIFY_CONSOLE_AUTH
@@ -15,13 +20,23 @@ function Get-DifyPluginPermission {
         throw "Failed to get plugin permission: $_"
     }
 
-    if (-not $Response.permission) {
-        throw "Failed to get plugin permission: Unexpected response"
+    if (Compare-SimpleVersion -Version $env:PSDIFY_VERSION -Ge "1.15.0") {
+        if (-not $Response.install_permission -or -not $Response.debug_permission) {
+            throw "Failed to get plugin permission: Unexpected response"
+        }
+        $Permission = [PSCustomObject]@{
+            InstallPermission = $Response.install_permission
+            DebugPermission   = $Response.debug_permission
+        }
     }
-
-    $Permission = [PSCustomObject]@{
-        InstallPermission = $Response.permission.install_permission
-        DebugPermission   = $Response.permission.debug_permission
+    else {
+        if (-not $Response.permission) {
+            throw "Failed to get plugin permission: Unexpected response"
+        }
+        $Permission = [PSCustomObject]@{
+            InstallPermission = $Response.permission.install_permission
+            DebugPermission   = $Response.permission.debug_permission
+        }
     }
 
     return $Permission
